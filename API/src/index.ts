9733ca33cd0746e5eec076ac5ec7f91825a6d7cf
Eduardo Matheus de Menezes Souto <eduardomatheusuesc@gmail.com>
2024-08-16 01:30:57 -0300

    feat: Implementation of Docker Compose

--- API/src/index.ts
+++ API/src/index.ts
@@ -1,13 +1,19 @@
+import path from 'node:path';
 import express from 'express';
 import mongoose from 'mongoose';
 
 import { router } from './router';
 
-mongoose.connect('mongodb://localhost:27017/waiterapp')
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/waiterapp';
+
+// mongoose.connect('mongodb://localhost:27017/waiterapp')
+// mongoose.connect('mongodb://host.docker.internal:27017/waiterapp')
+mongoose.connect(mongoUrl)
   .then(() => {
     const app = express();
     const port = 3001;
 
+    app.use('/uploads', express.static(path.resolve(__dirname, "..", "uploads")));
     app.use(express.json());
     app.use(router);
 
